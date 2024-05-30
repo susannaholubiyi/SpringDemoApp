9231d3a69d018d7a8ed24f2728ad84da9e265ce5
susannaholubiyi <susannaholubiyi@gmail.com>
2024-05-30 16:40:03 +0100

    example

--- src/main/java/application/services/MailGunMailService.java
+++ src/main/java/application/services/MailGunMailService.java
@@ -1,12 +1,24 @@
 package application.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
 @Primary
-@Repository
+@Service
 public class MailGunMailService implements MailService{
+
+    private  HelloService helloService;
+
+    @Autowired
+    public void setHelloService(HelloService helloService){
+        System.out.println("creating  mailGunMailService");
+        this.helloService = helloService;
+    }
     @Override
     public void sendMailTo(String email) {
+        helloService.sayHello(email);
         System.out.println("Mail delivered to "+ email + " using Mail gun");
     }
 }
